e10781119e50ebbc06cea47bddc2511cb5221ff3
mramos <mramos@frsf.utn.edu.ar>
2018-02-06 13:35:26 -0300

    Se crea Contest dentro del DAO
    WIP, comiteo para poder mergear

--- app/src/main/java/grupo_go_ra_ri/dam/isi/frsf/codeforces/dao/CodeForcesDaoHTTP.java
+++ app/src/main/java/grupo_go_ra_ri/dam/isi/frsf/codeforces/dao/CodeForcesDaoHTTP.java
@@ -4,6 +4,9 @@ import android.util.Log;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
+import grupo_go_ra_ri.dam.isi.frsf.codeforces.model.Contest;
 import grupo_go_ra_ri.dam.isi.frsf.codeforces.model.User;
 
 /**
@@ -64,5 +67,12 @@ public class CodeForcesDaoHTTP implements CodeForcesDao {
 
     @Override
     public void delete(User u) {}
+
+    public ArrayList<Contest> getContests() {
+        cliente = new MyGenericHTTPClient();
+        final String urlString = "http://codeforces.com/api/contest.list?gym=false";
+        String stringOfContests = cliente.performGetCall(urlString, null);
+        JSONArray arrayOfContests = new JSONObject(stringOfContests).getJSONArray();
+    }
 }
 
