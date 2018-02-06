c9c6388309baa984f2c1915256897436782d89eb
mramos <mramos@frsf.utn.edu.ar>
2018-02-06 15:43:40 -0300

    Se agrego el metodo para recuperar competencias adentro del metodo

--- app/src/main/java/grupo_go_ra_ri/dam/isi/frsf/codeforces/contest_list.java
+++ app/src/main/java/grupo_go_ra_ri/dam/isi/frsf/codeforces/contest_list.java
@@ -23,7 +23,7 @@ import grupo_go_ra_ri.dam.isi.frsf.codeforces.model.Contest;
 public class contest_list extends Fragment {
 
     private ListView lvCompetencias;
-    private ArrayList<Contest> lista_competencias;
+    private ArrayList<Contest> lista_competencias = new ArrayList<>();
     private adaptador_competencia adapter;
 
     @Override
@@ -35,10 +35,40 @@ public class contest_list extends Fragment {
         lvCompetencias = (ListView)view.findViewById(R.id.lv_contest);
 
         //Se carga la lista con los las competencias
-        lista_competencias = GetArrayItems();
+        //lista_competencias = GetArrayItems();
+        Thread thread = new Thread(new Runnable(){
+            @Override
+            public void run() {
+                try{
+                    MyGenericHTTPClient cliente = new MyGenericHTTPClient();
+                    final String urlString = "http://codeforces.com/api/contest.list?gym=false";
+                    String stringOfContests = cliente.performGetCall(urlString, null);
+                    System.out.println(stringOfContests);
+                    JSONObject obj = new JSONObject(stringOfContests);
+                    JSONArray contests = obj.getJSONArray("result");
+                    for(int i = 0; i < contests.length();i++){
+                        JSONObject c = contests.getJSONObject(i);
+                        System.out.println(c.get("phase"));
+                        if(c.get("phase").equals("BEFORE")){
+                            lista_competencias.add(new Contest( c.getInt("id"), c.getString("name"),
+                                    c.getBoolean("frozen"), c.getInt("durationSeconds"),
+                                    c.getInt("startTimeSeconds"), c.getInt("relativeTimeSeconds"),
+                                    c.getString("type")));
+                        }
+                    }
+                    System.out.println(lista_competencias);
+                    adapter = new adaptador_competencia(getActivity(),lista_competencias);
+                    lvCompetencias.setAdapter(adapter);
+                }
+                catch(Exception e) {
+                    Log.e("error", e.getMessage());
+                }
+            }
+        });
+        thread.start();
 
-        adapter = new adaptador_competencia(getActivity(),lista_competencias);
-        lvCompetencias.setAdapter(adapter);
+        //adapter = new adaptador_competencia(getActivity(),lista_competencias);
+        //lvCompetencias.setAdapter(adapter);
 
 
         return view;
