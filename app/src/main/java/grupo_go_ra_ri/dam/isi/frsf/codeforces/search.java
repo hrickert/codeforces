4264725b66f1284683df6b1afeca84b6452f1e13
Hernan Rickert <hernan_hjr@hotmail.com>
2018-02-06 15:46:41 -0300

    busca y pasa de pantalla pero no carga nada

--- app/src/main/java/grupo_go_ra_ri/dam/isi/frsf/codeforces/search.java
+++ app/src/main/java/grupo_go_ra_ri/dam/isi/frsf/codeforces/search.java
@@ -4,12 +4,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Looper;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import org.json.JSONObject;
@@ -31,34 +33,38 @@ public class search extends Fragment {
         daoCodeForces = new CodeForcesDaoHTTP();
         View view = inflater.inflate(R.layout.fragment_search, container, false);
 
+        final EditText handle = (EditText)view.findViewById(R.id.et_handle_search);
         Button btnSearch = (Button) view.findViewById(R.id.bt_search);
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            cliente = new MyGenericHTTPClient();
-            final String urlString = "http://codeforces.com/api/user.friends?onlyOnline=true";
-            ArrayList<User> users = new ArrayList<User>();
-            Thread thread = new Thread(new Runnable(){
-                @Override
+//                String user = handle.getText().toString();
+                cliente = new MyGenericHTTPClient();
+                ArrayList<User> users = new ArrayList<User>();
+                Thread thread = new Thread(new Runnable(){
+                    @Override
                     public void run() {
                         try {
-                            String userData = cliente.performGetCall(urlString, null);
+                            String url = "http://codeforces.com/api/user.info?handles="+handle.getText();
+//                            String urlString = "http://codeforces.com/api/user.friends?onlyOnline=true";
+//                            String userData = cliente.performGetCall(urlString, null);
+                            String userData = cliente.performGetCall(url, null);
                             JSONObject obj = new JSONObject(userData);
                             String status = (String) obj.getString("status");
-                            if (status == "200") {
+                            if (status.equals("OK")) {
                                 MenuSlideActivity.opcion = 5;
                                 Intent intencion = new Intent(getActivity().getApplicationContext(), MenuSlideActivity.class);
                                 startActivity(intencion);
                             } else {
-                                Toast toast1 = Toast.makeText(getActivity().getApplicationContext(), "Debe estar logueado", Toast.LENGTH_SHORT);
-                                toast1.show();
+                                Looper.prepare();
+                                Toast.makeText(getActivity().getApplicationContext(), "No hay ningun usuario con ese handle", Toast.LENGTH_SHORT).show();
                             }
                         } catch (Exception e) {
                             Log.e("error", e.getMessage());
                         }
-                }
-            });
-            thread.start();
+                    }
+                });
+                thread.start();
             // Esto está mal, porque se ejecuta el if antes de que la función de arriba retorne el objet
             // usuario. Hay que buscar como se ejecuta algo esperando la respuesta del "Thread" o hilo
             }
